f4e7d8ad181f6275bba321cc5bf61b2493b2f172
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-06 20:32:55 -0400

    main component of  snapshot module

---
+++ src/app/snapshot/pages/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page-snapshot',
+  templateUrl: 'main.component.html'
+})
+
+export class SnapshotComponent  {
+
+  title='SnapShotComponent';
+}
