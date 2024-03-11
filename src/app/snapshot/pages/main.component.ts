1c2a20a29bae294ce89790e4253764a954a1244e
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-10 20:06:22 -0400

    Creating the input propertie and output property on the father component.

--- src/app/snapshot/pages/main.component.ts
+++ src/app/snapshot/pages/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { imagePreview } from '../interfaces/previewImage.interface';
 
 @Component({
   selector: 'main-page-snapshot',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 export class SnapshotComponent  {
 
   title='SnapShotComponent';
+
+  /** Objeto que va almacenar los valores del preview de las imagenes. */
+
+  imagePrevieCars: imagePreview = {
+    carleftPreview: '',
+    carRightPreview: '',
+    carFrontPreview: '',
+    carBackPreview: ''
+  }
+
+  onNewPhoto(photo: imagePreview): void {
+    console.log(photo);
+    this.imagePrevieCars.carRightPreview = photo.carRightPreview;
+    this.imagePrevieCars.carleftPreview = photo.carleftPreview;
+    this.imagePrevieCars.carFrontPreview = photo.carFrontPreview;
+    this.imagePrevieCars.carBackPreview = photo.carBackPreview;
+  }
 }
