5f75fb13e816d81438cdc3cf46d0fa1a981d8760
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-12 17:35:55 -0400

    nav bar module main page of the module and component of the main page.

---
+++ src/app/navbar/pages/nabar-module.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'navbar-module',
+    templateUrl: 'navbar-module.component.html'
+})
+
+export class NavbarModuleComponent {
+    
+}
