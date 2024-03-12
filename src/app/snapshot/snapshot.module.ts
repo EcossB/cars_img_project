4e3c4a9472deafce95eaccf4de42bd468c2931a2
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-11 23:49:59 -0400

    adding new component

--- src/app/snapshot/snapshot.module.ts
+++ src/app/snapshot/snapshot.module.ts
@@ -8,11 +8,12 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import { WebcamModule } from 'ngx-webcam';
 import { PreviewImageComponent } from './components/preview-image/preview-image.component';
+import { CarDataComponent } from './components/car-data/car-data.component';
 
 
 
 @NgModule({
-  declarations: [ SnapshotComponent, TakeImageComponent, PreviewImageComponent],
+  declarations: [ SnapshotComponent, TakeImageComponent, PreviewImageComponent, CarDataComponent],
   exports:[
     SnapshotComponent
   ],
