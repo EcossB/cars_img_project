ef53d13c24a68482cc1c818a33ad264f89b43d17
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-18 14:38:32 -0400

    Creating a method to save the data of the image and also displaying the data of the vehicle in the table.

--- src/app/snapshot/pages/main.component.ts
+++ src/app/snapshot/pages/main.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { imagePreview } from '../interfaces/previewImage.interface';
+import { Vehicle } from '../interfaces/vehicle.interface';
+import { ApiCarsImgService } from '../services/api-cars-img.service';
+import { SaveVehiclePayload } from '../interfaces/DataImgPayload.interface';
 
 @Component({
   selector: 'main-page-snapshot',
@@ -11,6 +14,8 @@ export class SnapshotComponent  {
 
   title='SnapShotComponent';
 
+  constructor(public apiService: ApiCarsImgService){}
+
   /** Objeto que va almacenar los valores del preview de las imagenes. */
 
   imagePrevieCars: imagePreview = {
@@ -20,11 +25,67 @@ export class SnapshotComponent  {
     carBackPreview: ''
   }
 
+  saveimagePayload: SaveVehiclePayload = {
+    compania: '',
+    sucursal: '',
+    orden_Numero: 0,
+    img_lateral_derecho: '',
+    img_lateral_izquierdo: '',
+    img_frontal: '',
+    img_trasero: ''
+  }
+
+  vehicleObtained: Vehicle = {
+    compania: '',
+    sucursal: '',
+    orden_Numero: 0,
+    chasis: '',
+    marca: 0,
+    modelo: 0,
+    ano: 0,
+    placa: '',
+    color: 0
+  }
+
+
   onNewPhoto(photo: imagePreview): void {
     console.log(photo);
+    /*these properties are for the preview image */
     this.imagePrevieCars.carRightPreview = photo.carRightPreview;
     this.imagePrevieCars.carleftPreview = photo.carleftPreview;
     this.imagePrevieCars.carFrontPreview = photo.carFrontPreview;
     this.imagePrevieCars.carBackPreview = photo.carBackPreview;
+
+    /*these properties are for the payload to the api */
+    this.saveimagePayload.img_lateral_derecho = photo.carRightPreview;
+    this.saveimagePayload.img_lateral_izquierdo = photo.carleftPreview;
+    this.saveimagePayload.img_frontal = photo.carFrontPreview;
+    this.saveimagePayload.img_trasero = photo.carBackPreview;
   }
+
+
+  onNewDataTable(vehicle: Vehicle):void {
+    this.vehicleObtained = vehicle;
+
+    /*these properties are for the payload to the api */
+    this.saveimagePayload.compania = vehicle.compania;
+    this.saveimagePayload.sucursal = vehicle.sucursal;
+    this.saveimagePayload.orden_Numero = vehicle.orden_Numero;
+  }
+
+
+saveImages():void{
+  console.log(this.saveimagePayload)
+  this.apiService.SaveImagesVehicle(this.saveimagePayload).
+  subscribe({
+    next:(data: any) => {
+      console.log(data);
+    },
+    error:(error: any) => {
+      console.log(error);
+    }
+  })
+}
+
+
 }
