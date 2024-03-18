3a409fb33093d25eb97f0cdef30cbb5b841b30b1
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-18 14:33:45 -0400

    creating a output  propertie

--- src/app/snapshot/components/car-data/car-data.component.ts
+++ src/app/snapshot/components/car-data/car-data.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, TemplateRef } from '@angular/core';
 import { ApiCarsImgService } from '../../services/api-cars-img.service';
 import { chasis } from '../../interfaces/chasis.interface';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import {tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
+import { Vehicle } from '../../interfaces/vehicle.interface';
 
 @Component({
   selector: 'car-data',
@@ -12,11 +13,28 @@ import { Observable, Subject } from 'rxjs';
 })
 export class CarDataComponent implements OnInit{
 
-
 loading: boolean = false;
+
 chasis$!: Observable<chasis[]>;
+
 private searchTerms = new Subject<string>();
-chasisAvaible: chasis[] = [];
+
+selectedChasis: string = '';
+
+vehicleObtained: Vehicle = {
+  compania: '',
+  sucursal: '',
+  orden_Numero: 0,
+  chasis: '',
+  marca: 0,
+  modelo: 0,
+  ano: 0,
+  placa: '',
+  color: 0
+};
+
+@Output()
+public onNewDataTable: EventEmitter<Vehicle> = new EventEmitter;
 
 constructor(
   public apiService: ApiCarsImgService,
@@ -34,9 +52,10 @@ openModal(template: TemplateRef<void>): void{
 
 /**-------------------------------------------- */
 
-
-search(chasis: string) {
-
+search(chasis: string):void {
+  /** Im doing this because when we erase the input it throws a 400, so if the user erase 
+   * the whole thing on the input it wont throw a 400. 
+   */
   if(chasis.length > 0){
     this.searchTerms.next(chasis);
   } else
@@ -53,6 +72,36 @@ SearchChasis():void {
     tap((_) => (this.loading = false))
   );
 }
+
+
+getVehicleByChasis(chasis: string){
+  this.apiService.getVehicleByChasis(chasis).subscribe({
+    next: (vehicle: any) => {
+      console.log(vehicle);
+      this.vehicleObtained = vehicle;
+      this.onNewDataTable.emit(this.vehicleObtained);
+    },
+    error: (error) => {
+      console.log(error);
+    }
+  });
+}
+
+onSelected(event: any) {
+  var statev = event.target.textContent
+  this.selectedChasis = statev;
+  console.log(this.selectedChasis);
+}
+
+
+
+
+
+
+
+
+
+
 
 ngOnInit(): void {
   this.SearchChasis();
