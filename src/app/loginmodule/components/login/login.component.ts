3f5b25b4e4a68b634dfc82f8fa1fd551292c88f4
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:20:35 -0400

    Having a new method retriving the post endpoint login

--- src/app/loginmodule/components/login/login.component.ts
+++ src/app/loginmodule/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiCarsImgService } from '../../../snapshot/services/api-cars-img.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'module-login',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+  constructor(public apiService: ApiCarsImgService, public fb: FormBuilder, public router: Router){}
+
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+LogIn():void {
+  this.apiService.LoginUser(this.form)
+  .subscribe((response: any) =>{
+    console.log("Response", response)
+    localStorage.setItem('token', response.token);
+  });
+}
 
 }
