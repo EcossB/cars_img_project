3d11e985149f39078e128d1121554fd0747a82b7
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:18:48 -0400

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './loginmodule/components/login/login.component';
+import { SnapshotComponent } from './snapshot/pages/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', title: 'Login', component:LoginComponent},
+  {path: 'capturar', title: 'ProjecImg', component:SnapshotComponent},
+  {path: '**', title: 'ProjecImg', component:SnapshotComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
