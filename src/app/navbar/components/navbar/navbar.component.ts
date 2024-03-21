c5e840c8c7e80077c6e4e2a807f9bbbe923819c9
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-21 17:16:21 -0400

    Creating method log out, this method log out the user from the pool conection and also set the token to null

--- src/app/navbar/components/navbar/navbar.component.ts
+++ src/app/navbar/components/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../loginmodule/services/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { LoginResponse } from '../../../snapshot/interfaces/login.interface';
+import { ApiCarsImgService } from '../../../snapshot/services/api-cars-img.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +12,33 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(public authservice: AuthService, 
+    public fb: FormBuilder, 
+    public apiService: ApiCarsImgService,
+    public router: Router){}
+
+
+  form = {
+    userName: localStorage.getItem('userName'),
+    password: localStorage.getItem('password')
+  }
 
 logOut() {
-  
+  this.apiService.LogOutUser(this.form).subscribe({
+    next: (data:any) => {
+      console.log(data);
+    },
+    error: (error) => {
+      console.log(error);
+    }
+  })
+
+  localStorage.setItem('Token', '');
+  localStorage.setItem('userName', '');
+  localStorage.setItem('password', '');
+  this.authservice.currentUserSig.set(null);
+  this.router.navigateByUrl('/');
+
 }
 
 }
