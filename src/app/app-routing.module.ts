0132922a06baa8942bab65a75f9ca533717c284b
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-21 17:09:44 -0400

    Adding guards to path in router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule, Component, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './loginmodule/components/login/login.component';
 import { SnapshotComponent } from './snapshot/pages/main.component';
+import { AuthGuardService } from './loginmodule/services/auth-guard.service';
 
 const routes: Routes = [
   {path: '', title: 'Login', component:LoginComponent},
-  {path: 'capturar', title: 'ProjecImg', component:SnapshotComponent},
+  {path: 'capturar', title: 'ProjecImg', canActivate: [() => inject(AuthGuardService).canActivate()] , component:SnapshotComponent},
   {path: '**', title: 'ProjecImg', component:SnapshotComponent}
 ];
 
