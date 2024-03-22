301c3218c3c2fc4d2cadb3d79cbaffccc1068135
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-22 17:21:06 -0400

    adding a alert to handler server response when login

--- src/app/loginmodule/components/login/login.component.ts
+++ src/app/loginmodule/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { ApiCarsImgService } from '../../../snapshot/services/api-cars-img.servi
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { UpperCasePipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'module-login',
@@ -12,33 +12,49 @@ import { UpperCasePipe } from '@angular/common';
 })
 export class LoginComponent {
 
-
   constructor(public apiService: ApiCarsImgService,
      public fb: FormBuilder,
       public router: Router,
       public authservice: AuthService){}
 
+  Message: string = '';
+  alertType: string = '';
+  logIn: boolean = false;
 
   form = this.fb.nonNullable.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
   });
 
+  throwAlert(message: string, alert: string){
+    this.logIn = true;
+    this.Message = message;
+    this.alertType = alert;
+  }
+  setLocalStorage(username: string, password: string, token: string){
+    localStorage.setItem('userName', username);
+    localStorage.setItem('password', password);
+    localStorage.setItem('Token', token);
+  }
+
+
 LogIn():void {
   this.apiService.LoginUser(this.form)
   .subscribe({
     next:(response: any) => {
       console.log(response);
-      localStorage.setItem('userName', response.userName);
-      localStorage.setItem('password', response.password);
-      localStorage.setItem('Token', response.token);
+      this.setLocalStorage(response.userName, response.password, response.token);
       this.authservice.currentUserSig.set(response.userName);
-      this.router.navigateByUrl('/capturar');
+      this.throwAlert("Login succesfully", 'success');
+      setTimeout(() => {this.router.navigateByUrl('/capturar');}, 2000);
     },
-    error: (error: any) => {
+    error: (error: HttpErrorResponse ) => {
+      this.throwAlert(error.error.token, 'danger');
       console.log(error);
     }
   })
+
+  this.logIn = false;
 }
 
 }
