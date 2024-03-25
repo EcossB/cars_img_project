9b7120209a0a0333302952d0a997e50191112460
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-25 18:01:34 -0400

    interface for payload data that is coming from the server

---
+++ src/app/reportmodule/interface/imgCarData.interface.ts
@@ -0,0 +1,9 @@
+export interface ImgCarData {
+    compania: string,
+    sucursal: string,
+    orden_Numero: 0,
+    img_lateral_derecho: string,
+    img_lateral_izquierdo: string,
+    img_frontal: string,
+    img_trasero: string
+}
