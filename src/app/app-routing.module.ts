54b384a6279480f535832160d808f7f14bfec8c3
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-25 17:57:49 -0400

    adding new path to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule, Component, inject } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './loginmodule/components/login/login.component';
 import { SnapshotComponent } from './snapshot/pages/main.component';
 import { AuthGuardService } from './loginmodule/services/auth-guard.service';
+import { TablereportComponent } from './reportmodule/components/tablereport/tablereport.component';
+import { MainReportComponent } from './reportmodule/pages/main-report.component';
 
 const routes: Routes = [
   {path: '', title: 'Login', component:LoginComponent},
   {path: 'capturar', title: 'ProjecImg', canActivate: [() => inject(AuthGuardService).canActivate()] , component:SnapshotComponent},
-  {path: '**', title: 'ProjecImg', component:SnapshotComponent}
+  {path: '**', title: 'ProjecImg', component:SnapshotComponent},
+  {path: 'reporte', title: 'ProjecImg', canActivate: [() => inject(AuthGuardService).canActivate()] ,component:MainReportComponent}
 ];
 
 @NgModule({
