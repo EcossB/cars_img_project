5c816ae0090cde8723c57397de664267aab3d5eb
Erwing Coss <erwingcoss21@hotmail.com>
2024-04-04 18:41:12 -0400

    adding carousel and modal to the module

--- src/app/reportmodule/report.module.ts
+++ src/app/reportmodule/report.module.ts
@@ -3,19 +3,28 @@ import { CommonModule } from '@angular/common';
 import { MainReportComponent } from './pages/main-report.component';
 import { TablereportComponent } from './components/tablereport/tablereport.component';
 import { FormsModule } from '@angular/forms';
+
+
 import { AuthGuardService } from '../loginmodule/services/auth-guard.service';
 import { AuthService } from '../loginmodule/services/auth.service';
 import { ApiCarsImgService } from '../snapshot/services/api-cars-img.service';
 import { NavbarModule } from '../navbar/navbar.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { PdfMakeWrapper } from 'pdfmake-wrapper';
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
 
-
+PdfMakeWrapper.setFonts(pdfFonts);
 
 @NgModule({
   declarations: [MainReportComponent, TablereportComponent],
   imports: [
     CommonModule,
     FormsModule,
-    NavbarModule
+    NavbarModule,
+    ModalModule.forRoot(),
+    CarouselModule.forRoot(),
+    
   ],
   exports: [MainReportComponent, TablereportComponent],
   providers:[ApiCarsImgService, AuthService, AuthGuardService]
